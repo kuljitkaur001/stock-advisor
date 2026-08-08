729abd3ce0000def1714c401163e9fb8a631b4ee
kuljit kaur <112999864+kuljitkaur001@users.noreply.github.com>
2026-08-08 10:31:30 +0530

    Update API base URL for client

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: '/api/v1',
-  baseURL: 'https://stock-advisor-1-e3fb.onrender.com/api/v1',
+  baseURL: 'https://stock-advisor-bs6r.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
