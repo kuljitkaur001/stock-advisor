ab80616fbe62b353ee3bf0dad64dfcf645e11aaa
kuljit kaur <112999864+kuljitkaur001@users.noreply.github.com>
2026-08-08 09:39:35 +0530

    Update API base URL to production endpoints , url added - baseURL: 'https://stock-advisor-1-e3fb.onrender.com/api/v1',

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api/v1',
+  // baseURL: '/api/v1',
+  baseURL: 'https://stock-advisor-1-e3fb.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
